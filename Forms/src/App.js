0c893a8ade8850bdd928896ce5daf87265400150
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-12 19:24:44 +0530

    create a form for capture the last data or input that what we give using diffrent-diffrent dunction

--- Forms/src/App.js
+++ Forms/src/App.js
@@ -3,15 +3,36 @@ import './App.css';
 
 function App() {
   const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [yourEmail, setYourEmail] = useState('');
 
-  const changehandler = (event) => {
-    console.log("first Name is");
-    console.log(event.target.value);
+  const changeFirsthandler = (event) => {
+    // console.log("first Name is");
+    // console.log(event.target.value);
+    setFirstName(event.target.value);
+  };
+  
+  const changeLasthandler = (event) => {
+    // console.log("last Name is");
+    // console.log(event.target.value);
+    setLastName(event.target.value);
+  };
+  
+  const changeEmailhandler = (event) => {
+    // console.log("first Name is");
+    // console.log(event.target.value);
+    setYourEmail(event.target.value);
   };
 
   return (
     <div className='App'>
-      <input type='text' placeholder={"firstName"} onChange={changehandler} />
+      <input type='text' placeholder={"Your First Name"} onChange={changeFirsthandler} />
+      <br />
+      <br />
+      <input type='text' placeholder={"Your Last Name"} onChange={changeLasthandler} />
+      <br />
+      <br />
+      <input type='email' placeholder={"Enter your Email"} onChange={changeEmailhandler} />
     </div>
   );
 }
