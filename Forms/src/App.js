7a55b48452901c6d50406bfdbd24e60069e83e96
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-12 19:45:45 +0530

    this is the updated code which have a common function for capturing the last code

--- Forms/src/App.js
+++ Forms/src/App.js
@@ -2,37 +2,59 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [yourEmail, setYourEmail] = useState('');
+  // const [firstName, setFirstName] = useState('');
+  // const [lastName, setLastName] = useState('');
+  // const [yourEmail, setYourEmail] = useState('');
 
-  const changeFirsthandler = (event) => {
-    // console.log("first Name is");
-    // console.log(event.target.value);
-    setFirstName(event.target.value);
-  };
+  // const changeFirsthandler = (event) => {
+  //   // console.log("first Name is");
+  //   // console.log(event.target.value);
+  //   setFirstName(event.target.value);
+  // };
   
-  const changeLasthandler = (event) => {
-    // console.log("last Name is");
-    // console.log(event.target.value);
-    setLastName(event.target.value);
-  };
+  // const changeLasthandler = (event) => {
+  //   // console.log("last Name is");
+  //   // console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // };
   
-  const changeEmailhandler = (event) => {
-    // console.log("first Name is");
-    // console.log(event.target.value);
-    setYourEmail(event.target.value);
-  };
+  // const changeEmailhandler = (event) => {
+  //   // console.log("first Name is");
+  //   // console.log(event.target.value);
+  //   setYourEmail(event.target.value);
+  // };
+
+  const [formData, setFormData]= useState( {firstname: '', lastName: '', yourEmail: ''})
+  console.log(formData.yourEmail);
+  function changeHandler(event){
+    setFormData( prevFormaData =>{
+      return {
+        ...prevFormaData,
+        [event.target.name]: event.target.value
+      }
+    });
+  }
 
   return (
     <div className='App'>
-      <input type='text' placeholder={"Your First Name"} onChange={changeFirsthandler} />
+      <input 
+        type='text'
+        placeholder={"Your First Name"} 
+        onChange={changeHandler} 
+        name='firstName' />
       <br />
       <br />
-      <input type='text' placeholder={"Your Last Name"} onChange={changeLasthandler} />
+      <input
+        type='text'
+        placeholder={"Your Last Name"} 
+        onChange={changeHandler}
+        name='lastName' />
       <br />
       <br />
-      <input type='email' placeholder={"Enter your Email"} onChange={changeEmailhandler} />
+      <input type='email' 
+        placeholder={"Enter your Email"} 
+        onChange={changeHandler}
+        name='yourEmail' />
     </div>
   );
 }
