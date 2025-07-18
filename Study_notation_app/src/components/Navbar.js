ee885e4fa96ed02702e07ca4be143746a6b0fee6
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-18 14:40:59 +0530

    complete the nevbar section of the page without adding css but with functionality of routes

--- Study_notation_app/src/components/Navbar.js
+++ Study_notation_app/src/components/Navbar.js
@@ -1,9 +1,53 @@
 import React from "react";
+import logo from "../assets/Logo.svg";
+import {Link} from "react-router-dom";
 
 function Navbar(){
     return (
-        <div>Navbar</div>
-    )
+        <div className="flex">
+            <Link to="/">
+                 <img src={logo} alt='logo' widtg={160} height={32} loading="lazy" />
+            </Link>
+
+            <nav>
+                <ul className="flex">
+                    <li>
+                        <Link to = "/" >Home</Link>
+                    </li>
+                    <li>
+                        <Link to = "/" >About</Link>
+                    </li>
+                    <li>
+                        <Link to = "/" >Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <div>
+                {
+                    <Link to="/login">
+                        <button>
+                            Login
+                        </button>
+                    </Link>
+                }
+                {
+                    <Link to="/signup">
+                        <button>
+                            Signup
+                        </button>
+                    </Link>
+                }
+                {
+                    <Link to="/">
+                        <button>
+                            Log out
+                        </button>
+                    </Link>
+                }
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
