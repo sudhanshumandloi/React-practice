515cb40703ba0454aedfdb1149c225258cc7c559
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-18 23:02:46 +0530

    added name inputes

--- Study_notation_app/src/components/Signupform.js
+++ Study_notation_app/src/components/Signupform.js
@@ -15,6 +15,37 @@ const Signupform = () => {
                     <button>Student</button>
                     <button>Instructor</button>
                 </div>
+                
+                 {/* firstname - lastname */}
+                <div>
+                    <label>
+                        <p>First Name<sub>*</sub></p>
+                        <input required
+                            type="text"
+                            name="firstname"
+                            onChange={changeHandler}
+                            placeholder="Enter your first name"
+                            value={formData.firstname}
+                        />
+                    </label>
+                    
+                    <label>
+                        <p>Last Name<sub>*</sub></p>
+                        <input required
+                            type="text"
+                            name="lastname"
+                            onChange={changeHandler}
+                            placeholder="Enter your last name"
+                            value={formData.lastname}
+                        />
+                    </label>
+                </div>
+
+
+                {/* password - confirm password */}
+                <div>
+
+                </div>
 
 
             </form>
