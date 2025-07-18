da570c8b3c31a1a1688154d65d3afd472d4cff13
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-18 13:47:08 +0530

    make routes in app.js file and test it

--- Study_notation_app/src/App.js
+++ Study_notation_app/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Deshbord from "./pages/Deshboard";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashbord" element={<Deshbord />} />
+
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ Study_notation_app/src/pages/Deshboard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Deshbord(){
+    return (
+        <div>Deshbord</div>
+    )
+}
+
+export default Deshbord;
