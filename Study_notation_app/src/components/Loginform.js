f1065065fc7995f77a4e88ac04e8b49ab7670bae
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-18 19:38:28 +0530

    make input fieald for ithe login

--- Study_notation_app/src/components/Loginform.js
+++ Study_notation_app/src/components/Loginform.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Loginform = () => {
+    const [formData, setFormdata] = useState({email:'', password:'' });
+
+    const [showPassword, serShowPassword] = useState(false);
+
+    function changeHandler(event){
+        setFormdata( prevData => (
+            {
+                ...prevData,
+                [event.target.name]: event.target.value
+            }
+         ) )
+    }
+
     return (
-        <div>Loginform</div>
+        <div>
+            <form>
+                <label>
+                    <p>
+                        Email Address<sub>*</sub>
+                    </p>
+                    <input required
+                        type="email"
+                        value={formData.email}
+                        onChange={changeHandler}
+                        placeholder="Enter email id"/>
+                </label>
+
+                
+                <label>
+                    <p>
+                        Password<sub>*</sub>
+                    </p>
+                    <input required
+                        type={showPassword ? ("text") : ("password")}
+                        value={formData.email}
+                        onChange={changeHandler}
+                        placeholder="Enter email id"/>
+                </label>
+            </form>
+        </div>
     )
 }
 
