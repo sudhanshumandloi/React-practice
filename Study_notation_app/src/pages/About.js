42af56f9482e3202f1f6e6e5356b89a41e6469b5
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-23 00:40:58 +0530

    create about and contact page

---
+++ Study_notation_app/src/pages/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home(){
+    return (
+        <div className="flex justify-center items-center text-white text-3xl h-full">About</div>
+    )
+}
+
+export default Home;
---
+++ Study_notation_app/src/pages/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home(){
+    return (
+        <div className="flex justify-center items-center text-white text-3xl h-full">Contact</div>
+    )
+}
+
+export default Home;
