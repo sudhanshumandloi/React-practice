0379ee43f4718cc3029178aaa4ef4ece3f7fc125
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-22 17:59:51 +0530

    added justify-between in top level div

--- Study_notation_app/src/components/Template.js
+++ Study_notation_app/src/components/Template.js
@@ -6,7 +6,7 @@ import {FcGoogle} from "react-icons/fc"
 
 function Template({title, desc1, desc2, image, formtyp, setIsLoggedIn}){
     return(
-        <div className="flex w-11/12 max-w-[1160px] py-12 mx-auto gap-x-12 gap-y-0">
+        <div className="flex w-11/12 justify-between max-w-[1160px] py-12 mx-auto gap-x-12 gap-y-0">
             <div>
                 <h1 className="text-richblack-5 font-semibold text-[1.875rem] leading-[2.375rem]">
                     {title}
