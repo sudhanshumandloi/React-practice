8ba80e37555e0001f216c0497c8c6c4cf1316644
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-22 23:53:02 +0530

    remove the problem dirct accsess of dashbord page

--- Study_notation_app/src/components/Privateraoute.js
+++ Study_notation_app/src/components/Privateraoute.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
-function Privateroute(){
-    return
+function Privateroute( {isLoggedIn, children}){
+    if(isLoggedIn){
+        return children
+    } else{
+        return <Navigate to="/login"/>
+    }
 }
 
 export default Privateroute;
