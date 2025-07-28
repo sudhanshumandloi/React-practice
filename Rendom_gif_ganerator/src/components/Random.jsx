3f463d8446bdba8e28f2198ef0dde36604175cbe
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-28 17:58:03 +0530

    create a api key for the gif

--- Rendom_gif_ganerator/src/components/Random.jsx
+++ Rendom_gif_ganerator/src/components/Random.jsx
@@ -13,7 +13,7 @@ export default function Random(){
         <div className="bg-green-500 w-1/2 rounded-md h-[450px] border border-black 
         flex flex-col mt-[15px] items-center gap-y-5">
             <h1 className="text-2xl underline font-bold mt-[20px]">A RANDOM GIF</h1>
-            <img src={gif} alt="gif" />
+            <img src={gif} alt="gif" width={450}/>
             <button onClick={clickHandler}
                 className="w-10/12 text-lg py-2 rounded-lg bg-white opacity-60"
             >
