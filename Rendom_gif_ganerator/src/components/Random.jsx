c481ca5adc976858efad04bf1a632d08fcabe5db
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-28 17:26:08 +0530

    adde a componenet of random gif in and set the css of it

--- Rendom_gif_ganerator/src/components/Random.jsx
+++ Rendom_gif_ganerator/src/components/Random.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 
 export default function Random(){
+
+    const [gif, setGif] = useState("");
+
     return(
-        <div>Random</div>
+        <div className="bg-green-500 w-1/2 rounded-md h-[450px] border border-black 
+        flex flex-col mt-[15px] items-center gap-y-5">
+            <h1>A RANDOM GIF</h1>
+            <img src={gif} alt="gif" />
+            <button>GENERATE</button>
+        </div>
     );
 }
