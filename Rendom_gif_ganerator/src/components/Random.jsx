5743382aba464d3e9f6aeea1e51ef37a52b7b8bd
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-28 19:12:49 +0530

    set loading spinner in the component

--- Rendom_gif_ganerator/src/components/Random.jsx
+++ Rendom_gif_ganerator/src/components/Random.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import Spinner from "./Spinner"
 
 export default function Random(){
 
     const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
     const [gif, setGif] = useState("");
+    const [loading, setLoading] = useState('');
 
     async function fetchData(){
+        setLoading(true);
         const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
         const {data} = await axios.get(url);
         const imageSource = data.data.images.downsized_large.url;
         setGif(imageSource);
+        setLoading(false);
     }
 
     useEffect( () =>{
@@ -27,7 +31,9 @@ export default function Random(){
         <div className="bg-green-500 w-1/2 rounded-md  border border-black 
         flex flex-col mt-[15px] items-center gap-y-5">
             <h1 className="text-2xl underline font-bold mt-[20px]">A RANDOM GIF</h1>
-            <img src={gif} alt="gif" width={450}/>
+            {
+                loading ? (<Spinner />) : (<img src={gif} alt="gif" width={450}/>)
+            }
             <button onClick={clickHandler}
                 className="w-10/12 text-lg py-2 rounded-lg bg-white opacity-60 mb-[20px]"
             >
