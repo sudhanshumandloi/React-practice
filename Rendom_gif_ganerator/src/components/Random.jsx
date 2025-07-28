c519150fb32ac26269d0d8e37a44715b10de5301
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-28 17:35:33 +0530

    set the css of ganerate button and set the function fot cilck button

--- Rendom_gif_ganerator/src/components/Random.jsx
+++ Rendom_gif_ganerator/src/components/Random.jsx
@@ -4,12 +4,21 @@ export default function Random(){
 
     const [gif, setGif] = useState("");
 
+    function clickHandler() {
+
+    }
+    
+
     return(
         <div className="bg-green-500 w-1/2 rounded-md h-[450px] border border-black 
         flex flex-col mt-[15px] items-center gap-y-5">
             <h1 className="text-2xl underline font-bold mt-[20px]">A RANDOM GIF</h1>
             <img src={gif} alt="gif" />
-            <button>GENERATE</button>
+            <button onClick={clickHandler}
+                className="w-10/12 text-lg py-2 rounded-lg bg-white opacity-60"
+            >
+                GENERATE
+            </button>
         </div>
     );
 }
