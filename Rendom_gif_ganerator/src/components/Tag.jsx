0cd9d838f1643c50aeff78a05397ad44dfcb0981
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-28 19:18:56 +0530

    copy the code from random file and past it in tag file

--- Rendom_gif_ganerator/src/components/Tag.jsx
+++ Rendom_gif_ganerator/src/components/Tag.jsx
@@ -1,6 +1,43 @@
+import Spinner from "./Spinner";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Tag(){
+        const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+    const [gif, setGif] = useState("");
+    const [loading, setLoading] = useState('');
+
+    async function fetchData(){
+        setLoading(true);
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+        const {data} = await axios.get(url);
+        const imageSource = data.data.images.downsized_large.url;
+        setGif(imageSource);
+        setLoading(false);
+    }
+
+    useEffect( () =>{
+        fetchData();
+    }, [] )
+
+    function clickHandler() {
+        fetchData();
+    }
+    
+
     return(
-        <div>Tag</div>
-    )
+        <div className="bg-green-500 w-1/2 rounded-md  border border-black 
+        flex flex-col mt-[15px] items-center gap-y-5">
+            <h1 className="text-2xl underline font-bold mt-[20px]">A RANDOM GIF</h1>
+            {
+                loading ? (<Spinner />) : (<img src={gif} alt="gif" width={450}/>)
+            }
+            <button onClick={clickHandler}
+                className="w-10/12 text-lg py-2 rounded-lg bg-white opacity-60 mb-[20px]"
+            >
+                GENERATE
+            </button>
+        </div>
+    );
 }
