ef16c1bced6f5c786cfb29020db071c2bc5f1d43
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-28 18:05:23 +0530

    satrting implimenting the api key

--- Rendom_gif_ganerator/src/components/Random.jsx
+++ Rendom_gif_ganerator/src/components/Random.jsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 export default function Random(){
 
+    const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
     const [gif, setGif] = useState("");
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
     function clickHandler() {
 
