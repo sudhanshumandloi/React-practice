06ba12306601061e1fd78c1278bdb956d69a6142
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-28 17:12:56 +0530

    complete the heading of random gif

--- Rendom_gif_ganerator/src/App.jsx
+++ Rendom_gif_ganerator/src/App.jsx
@@ -1,3 +1,16 @@
+import Random from "./components/Random";
+import Tag from "./components/Tag";
+
 export default function App() {
-  return <div>Random GIF</div>;
+  return (
+    <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
+      <h1 className="bg-white rounded-lg w-11/12 text-center mt-[40px] px-10 py-2 text-4xl font-bold">
+        RANDOM GIFS
+      </h1>
+      <div className="flex flex-col">
+        <Random />
+        <Tag />
+      </div>
+    </div>
+  );
 }
