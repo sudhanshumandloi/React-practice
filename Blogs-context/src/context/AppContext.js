ad7d7826ca521cc78eb68891270ab17d3fc794c5
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-08-23 18:29:11 +0530

    fix the error and display the different states

--- Blogs-context/src/context/AppContext.js
+++ Blogs-context/src/context/AppContext.js
@@ -1,7 +1,26 @@
-import { createContext } from "react";
+import { use } from "react";
+import { createContext, useState } from "react";
 
 export const AppContext = createContext();
 
 function AppContextProvider({children}){
-    
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(null);
+
+    const value = {
+        loading,
+        setLoading,
+        posts,
+        setPosts,
+        page,
+        setPage,
+        totalPages,
+        setTotalPages
+    };
+
+    return <AppContext.Provider value={value}>{children}</AppContext.Provider>
 }
+
+export default AppContextProvider;
