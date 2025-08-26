bef66915578614c36fa18045e962415f22761912
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-08-27 01:12:58 +0530

    specify the card component

--- Blogs-context/src/components/Card.jsx
+++ Blogs-context/src/components/Card.jsx
@@ -1,4 +1,20 @@
 
-export default function Card(){
-    return 
+export default function Card(post){
+    return (
+        <div>
+            <div key={post.id}>
+                <p>{post.title}</p>
+                <p>
+                    By<span>{post.author}</span>on<span>{post.category}</span>
+                </p>
+                <p>Posted on{post.date}</p>
+                <p>{post.content}</p>
+                <div>
+                    {post.tag.map( (tag, index) => {
+                        return <span key={index}>{`#${tag}`}</span>
+                    })}
+                </div>
+            </div>
+        </div>
+    );
 }
