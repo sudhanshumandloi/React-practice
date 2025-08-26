347aa6ac389bcb6f3b1c252baf5ec970df61b72b
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-08-27 01:02:41 +0530

    fetch the api from app file

--- Blogs-context/src/App.js
+++ Blogs-context/src/App.js
@@ -1,8 +1,17 @@
+import { useContext, useEffect } from "react";
 import Blogs from "./components/Blogs";
 import Header from "./components/Header";
 import Pagination from "./components/Pagination";
+import { AppContext } from "./context/AppContext";
 
 export default function App() {
+
+  const {fetchBlogPosts} = useContext(AppContext);
+
+  useEffect(() => {
+    fetchBlogPosts();
+  },[]);
+
   return( 
     <div>
       <Header />
