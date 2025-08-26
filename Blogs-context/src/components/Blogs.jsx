c8fdce2a823908de037ac62bb5f3da792594bd74
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-08-27 01:23:54 +0530

    added thr card code to Blog componrnt without using componenet

--- Blogs-context/src/components/Blogs.jsx
+++ Blogs-context/src/components/Blogs.jsx
@@ -15,7 +15,21 @@ export default function Blogs(){
                         ) : ( posts.length === 0 ? 
                                 (<div>
                                     <p>No post found</p>
-                                </div>) : (posts.map( (post) => (<Card />) ) )
+                                </div>) : (posts.map( (post) => (
+                                    <div key={post.id}>
+                                        <p>{post.title}</p>
+                                        <p>
+                                            By<span>{post.author}</span>on<span>{post.category}</span>
+                                        </p>
+                                        <p>Posted on{post.date}</p>
+                                        <p>{post.content}</p>
+                                        <div>
+                                            {post.tags.map( (tag, index) => {
+                                                return <span key={index}>{`#${tag}`}</span>
+                                            })}
+                                        </div>
+                                    </div>
+                            ) ) )
                 )
             }
         </div>
