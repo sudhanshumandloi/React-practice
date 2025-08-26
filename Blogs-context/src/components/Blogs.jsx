30fa94e0baf40134710fc299a48d19aa8d85d778
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-08-27 00:07:02 +0530

    make context for loading in blog componenet

--- Blogs-context/src/components/Blogs.jsx
+++ Blogs-context/src/components/Blogs.jsx
@@ -1,4 +1,15 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 export default function Blogs(){
-    return
+
+    const {loading} = useContext(AppContext);
+
+    return(
+        <div>
+            {
+                loading ? (<div className="items-center justify-center font-bold text-xl">Loading</div>) : ()
+            }
+        </div>
+    );
 }
