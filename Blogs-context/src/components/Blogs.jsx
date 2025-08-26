ef56bf218e7edb7d722ea0a74435dfa857da74ad
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-08-27 00:21:18 +0530

    the condition is applyed

--- Blogs-context/src/components/Blogs.jsx
+++ Blogs-context/src/components/Blogs.jsx
@@ -1,14 +1,22 @@
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import Card from "./Card";
 
 export default function Blogs(){
 
-    const {loading} = useContext(AppContext);
+    const {loading, posts} = useContext(AppContext);
 
     return(
         <div>
             {
-                loading ? (<div className="items-center justify-center font-bold text-xl">Loading</div>) : ()
+                loading ? (<div className="items-center justify-center font-bold text-xl">
+                                Loading
+                            </div>
+                        ) : ( posts.length === 0 ? 
+                                (<div>
+                                    <p>No post found</p>
+                                </div>) : (posts.map( (post) => (<Card />) ) )
+                )
             }
         </div>
     );
