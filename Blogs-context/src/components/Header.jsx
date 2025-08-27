0dbc85bf6aa4d31e028aabba3b26790885146677
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-08-28 00:09:41 +0530

    coplete the css in header component

--- Blogs-context/src/components/Header.jsx
+++ Blogs-context/src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 export default function Header(){
     return (
-        <div className="border shadow-md py-2">
+        <div className="w-full border shadow-md py-2">
             <h1 className="text-xl uppercase text-center font-bold">Codehepl Blogs</h1>
         </div>
     );
