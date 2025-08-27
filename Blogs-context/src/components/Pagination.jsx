9d647d45c061a6ac6d77b937d8257fd0fed15243
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-08-27 23:39:57 +0530

    add code for next button in pagination component

--- Blogs-context/src/components/Pagination.jsx
+++ Blogs-context/src/components/Pagination.jsx
@@ -3,13 +3,17 @@ import { AppContext } from "../context/AppContext";
 
 export default function Pagination(){
 
-    const {page, handlePageChange} = useContext(AppContext);
+    const {page, handlePageChange, totalPages} = useContext(AppContext);
 
     return(
         <div>
             {
                 page > 1 &&
                 (<button onClick={handlePageChange(page-1)}>Previous</button>)
+            }
+            {
+                page < totalPages && 
+                (<button onClick={handlePageChange(page+1)}>Next</button>)
             }
         </div>
     );
