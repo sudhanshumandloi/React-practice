06d354c65251769031c7b2e9f22e18916b3aad43
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-11-02 22:01:12 +0530

    adjust the page text and contain size in blogs-context project

--- Blogs-context/src/components/Blogs.jsx
+++ Blogs-context/src/components/Blogs.jsx
@@ -6,7 +6,7 @@ export default function Blogs(){
     const {loading, posts} = useContext(AppContext);
 
     return(
-        <div className=" w-11/12 py-4 max-w-[550px] flex flex-col gap-y-7 mt-[50px]">
+        <div className=" w-11/12 py-4 max-w-[650px] flex flex-col gap-y-7 mt-[50px] ">
             {
                 loading ? (<div className=" ">
                                 Loading
@@ -16,15 +16,15 @@ export default function Blogs(){
                                     <p>No post found</p>
                                 </div>) : (posts.map( (post) => (
                                     <div key={post.id}>
-                                        <p className="font-bold text-xs">{post.title}</p>
-                                        <p className="text-[11px]">
+                                        <p className="font-bold text-lg">{post.title}</p>
+                                        <p className="text-[12px]">
                                             By<span className="italic">{post.author}</span>on<span className="underline font-bold">{post.category}</span>
                                         </p>
-                                        <p className="text-[11px] ">Posted on{post.date}</p>
-                                        <p className="text-[12px] mt-[10px]">{post.content}</p>
+                                        <p className="text-[12px] ">Posted on{post.date}</p>
+                                        <p className="text-[14px] mt-[10px]">{post.content}</p>
                                         <div className="flex gap-x-3">
                                             {post.tags.map( (tag, index) => {
-                                                return <span key={index} className="text-blue-500 underline font-bold text-[11px]">{`#${tag}`}</span>
+                                                return <span key={index} className="text-blue-500 underline font-bold text-[14px]">{`#${tag}`}</span>
                                             })}
                                         </div>
                                     </div>
--- Blogs-context/src/components/Pagination.jsx
+++ Blogs-context/src/components/Pagination.jsx
@@ -6,18 +6,24 @@ export default function Pagination(){
     const {page, handlePageChange, totalPages} = useContext(AppContext);
 
     return(
-        <div>
-            {
+        <div className="w-full flex justify-center items-center border-2">
+        <div className="flex justify-between w-11/12 max-w-[650px] py-2">
+            <div className="flex gap-x-3">
+                {
                 page > 1 &&
-                (<button onClick={() => handlePageChange(page-1)}>Previous</button>)
+                (<button className="rounded-md border-2 px-2 py-1"  
+                    onClick={() => handlePageChange(page-1)}>Previous</button>)
             }
             {
                 page < totalPages && 
-                (<button onClick={() => handlePageChange(page+1)}>Next</button>)
+                (<button className="rounded-md border-2 px-2 py-1"
+                    onClick={() => handlePageChange(page+1)}>Next</button>)
             }
-            <p>
+            </div>
+            <p className="font-bold text-sm">
                 page {page} of {totalPages}
             </p>
+        </div>
         </div>
     );
 }
