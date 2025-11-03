b64e9a80c913c30f36fa21c44cc3cd1121b7024f
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-11-03 19:11:09 +0530

    add routes for navigation in app file

--- Blogs-context/src/App.js
+++ Blogs-context/src/App.js
@@ -4,6 +4,11 @@ import Blogs from "./components/Blogs";
 import Header from "./components/Header";
 import Pagination from "./components/Pagination";
 import { AppContext } from "./context/AppContext";
+import {Route, Routes} from "react-router-dom";
+import Home from './components/Home';
+import BlogePage from './components/BlogPage';
+import TagPage from './components/TagPage';
+import CategoryPage from './components/CategoryPage';
 
 export default function App() {
 
@@ -14,10 +19,17 @@ export default function App() {
   },[]);
 
   return( 
-    <div className='w-full h-full flex flex-col gap-y-1 justify-center items-center'>
+    <Routes>
+      <Route path='/' element = {<Home />}/>
+      <Route path='/blog/:blogId' element = {<BlogePage />}/>
+      <Route path='/tags/:tag' element = {<TagPage />}/>
+      <Route path='/categories/:category' element = {<CategoryPage />}/>
+    </Routes>
+    
+  );
+}
+<div className='w-full h-full flex flex-col gap-y-1 justify-center items-center'>
       <Header />
       <Blogs />
       <Pagination />
     </div>
-  );
-}
